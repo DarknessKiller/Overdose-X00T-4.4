1b0f89c24602d6190669aeaa837a7b1f850d7ee2
DarknessKiller <23330152+DarknessKiller@users.noreply.github.com>
2021-08-17 14:18:29 +0800

    Revert: all disabled logging
    * Let keep this active so that we can debug if required on release build.

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -3257,7 +3257,6 @@ out:
 	return match;
 }
 
-#ifdef CONFIG_AUDIT
 static int (*aurule_callback)(void) = audit_update_lsm_rules;
 
 static int aurule_avc_callback(u32 event)
@@ -3280,7 +3279,6 @@ static int __init aurule_init(void)
 	return err;
 }
 __initcall(aurule_init);
-#endif
 
 #ifdef CONFIG_NETLABEL
 /**
